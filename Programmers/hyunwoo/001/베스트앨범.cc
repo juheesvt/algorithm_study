70b2a1137d5e470db24817e4cb1a599733be6106
hwPark <phphww93@gmail.com>
2023-07-23 11:07:26 +0900

    베스트앨범 temp

---
+++ Programmers/hyunwoo/001/베스트앨범.cc
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+
+using namespace std;
+
+struct customFn {
+    bool operator()(const int lhs, const int rhs) const {
+        return lhs > rhs;
+    }
+};
+// 53점 score
+vector<int> solution(vector<string> genres, vector<int> plays) {
+    vector<int> answer;
+    map<string,int> genresScore;
+    multimap<int,string,customFn> totalScore;
+    unordered_map<string, multimap<int,int,customFn>> bestAlbum;
+    
+    for(int i = 0; i < genres.size(); i++) {
+        genresScore[genres.at(i)] += plays.at(i);
+        bestAlbum[genres.at(i)].insert(std::make_pair(plays.at(i), i));
+    }
+    // 1. 속한 노래가 많이 재생된 장르 정렬
+    for(auto iter = genresScore.begin(); iter != genresScore.end(); iter++) {
+        totalScore.emplace((*iter).second, (*iter).first);
+    }
+    // 2. 장르 내에서 많이 재생된 노래를 먼저 수록(두개)
+    for(auto iter = totalScore.begin(); iter != totalScore.end(); iter++) {
+        auto biter = bestAlbum.find((*iter).second)->second.begin();
+        auto biter_next = bestAlbum.find((*iter).second)->second.begin()++;
+        for(int i = 0; i < 2; i++) {
+            if((*biter).first == (*biter_next).first) {
+                answer.push_back(min((*biter).second, (*biter_next).second));
+            }
+            else {
+                answer.push_back((*biter).second);
+            }
+            biter++;
+            biter_next++;
+        }
+    }
+    
+    return answer;
+}
