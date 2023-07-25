6f20ea2a45d49b95ff2f61a3dcd02ac5db5d35d8
ParkHyunA <122820408+gusdk45@users.noreply.github.com>
2023-07-25 12:38:21 +0900

    Update 기능개발.cpp

--- Programmers/hyuna001/기능개발.cpp
+++ Programmers/hyuna001/기능개발.cpp
@@ -6,24 +6,22 @@ using namespace std;
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
 
-    int n = progresses.size(); // 작업의 개수
-    int idx = 0; // 배포될 작업들을 가리키는 인덱스
+    int n = progresses.size(); 
+    int idx = 0; 
 
     while (idx < n) {
-        int day = 0; // 배포되는 날짜
-        // 현재 인덱스의 작업이 배포될 때까지 걸리는 일 수 계산
+        int day = 0; 
         while (progresses[idx] + (speeds[idx] * day) < 100) {
             day++;
         }
 
-        int deploy_count = 0; // 배포되는 기능의 개수
-        // 배포될 작업들을 찾고, 배포되는 기능의 개수를 세기
+        int deploy_count = 0; 
         while (idx < n && progresses[idx] + (speeds[idx] * day) >= 100) {
             deploy_count++;
             idx++;
         }
 
-        answer.push_back(deploy_count); // 배포되는 기능의 개수를 결과에 추가
+        answer.push_back(deploy_count); 
     }
 
     return answer;
